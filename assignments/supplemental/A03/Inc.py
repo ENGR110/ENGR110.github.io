fe7efbd1df92249e3af4309d6e80f081d3910cc9
hoosieree <ashroyer@iu.edu>
2019-02-11 13:33:42 +0000

    add incrementer and testbench

---
+++ assignments/supplemental/A03/Inc.py
@@ -0,0 +1,14 @@
+from myhdl import always_seq, block
+
+@block
+def Inc(count, enable, clock, reset):
+    # count: output
+    # enable: control, increment when 1 else retain last value
+    # clock: input
+    # reset: asynchronous reset input
+    @always_seq(clock.posedge, reset=reset)
+    def seq():
+        if enable:
+            count.next = count+1
+
+    return seq
