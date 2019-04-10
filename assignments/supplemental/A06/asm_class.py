ce7387ad600a9e0eabd99ca7ae1180899f67e592
hoosieree <ashroyer@gmail.com>
2019-04-09 21:48:15 -0400

    removing whitespace earlier
    makes the rest of the steps much simpler

--- assignments/supplemental/A06/asm_class.py
+++ assignments/supplemental/A06/asm_class.py
@@ -1,6 +1,5 @@
-class assembler(object):
-    def __init__(x, name):
-        x.name = name
+class assembler:
+    def __init__(x):
         x.lines = []
         x.st = {
             'SCREEN': 0x4000,
@@ -12,55 +11,30 @@ class assembler(object):
     def read_asm(self, filename):
         with open(filename) as f:
             for line in f:
-                self.lines.append(line)
+                self.lines.append(line.strip())
 
     def get_labels(self):
         icount = 0
         for line in self.lines:
             if self.is_instruction(line):
                 icount += 1
-            if self.is_label(line):
-                self.st[self.only_label(line)] = icount
+            if line[0] == '(':
+                self.st[line[1:-1]] = icount
 
     def get_vars(self):
         vcount = 16
         for line in self.lines:
-            if line[1:-1] not in self.st:
+            if line[1:] not in self.st:
                 if line[0] == '@':
-                    self.st[line[1:-1]] = vcount
+                    self.st[line[1:]] = vcount
                     vcount += 1
 
     def is_instruction(self, line):
-        if line.isspace():
-            return False
-        for c in line:
-            if c.isspace() and c != '/' and c != '(':
-                return True
-            if c == '/' or c == '(':
-                return False
-        # return None
-
-    def only_label(self, line):
-        # given "     (label)    ", return "label"
-        # "    (LOOP)   "
-        return line[1:-2]
-
-    def is_label(self, line):
-        # TODO watch out for commented-out lines
-        #  //    this is (a comment)
-        return '(' in line
-
-# st: {
-# (should still have SCREEN, etc.)
-# 'i':         16,
-# 'counter':   17,
-# 'hello':     18,
-# 'this_has:special$characters':   19
-# }
+        return line[0] != '/' and line[0] != '('
 
 if __name__ == "__main__":
     filename = 'tests/vars.asm'
-    a = assembler('a')
+    a = assembler()
     a.read_asm(filename)
     # for line in a.lines:
     #     print(a.is_instruction(line))
