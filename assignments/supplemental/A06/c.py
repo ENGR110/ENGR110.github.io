fd62e2c5b30bc5987edd4aad3ae4f15ca4d04580
ENGR <ashroyer@iu.edu>
2019-04-15 18:12:29 -0400

    add stuff

---
+++ assignments/supplemental/A06/c.py
@@ -0,0 +1,14 @@
+import sys
+
+f = sys.argv[-1]
+naive_passing_output = [
+    1111110000010000,
+    1110001100000110,
+    1110001100001000,
+    1110110000010000,
+    1110001100001000,
+    1110101010000111,
+]
+
+for line in naive_passing_output:
+    print(line)
