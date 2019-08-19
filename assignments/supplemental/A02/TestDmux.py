aacd46d7d7d84cd3ab8d45fd3e97dbad828acf8b
Alex Shroyer <ashroyer@gmail.com>
2019-08-19 11:31:51 -0400

    remove "Dmux" and keep "DMux"

--- assignments/supplemental/A02/TestDmux.py
+++
@@ -1,9 +0,0 @@
-from Test import TestGate, unittest, import_module
-
-class TestDmux(TestGate.TestGate):
-    def setUp(self):
-        self.gate = 'Dmux'
-        self.m = import_module(self.gate)
-
-if __name__ == '__main__':
-    unittest.main()
